d8872ce2d3132dd5973af118c2fd8f43af2c507f
salemij93 <salemi-j@hotmail.com>
2021-02-13 12:50:11 -0600

    added DeleteCommand undo and redo

--- src/main/DeleteCommand.java
+++ src/main/DeleteCommand.java
@@ -1,9 +1,20 @@
 package main;
 
-public class DeleteCommand implements ICommand{
+public class DeleteCommand implements ICommand, IUndoRedo{
 
     @Override
     public void runCommand() {
         System.out.println("delete");
+        CommandHistory.add(this);
+    }
+
+    @Override
+    public void undoCommand() {
+        System.out.println("undo delete");
+    }
+
+    @Override
+    public void redoCommand() {
+        System.out.println("redo delete");
     }
 }
