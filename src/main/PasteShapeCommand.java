c345116923c2914bb0bb85ddcb0ab348a9046a64
salemij93 <salemi-j@hotmail.com>
2021-02-14 14:22:17 -0600

    adding undo and redo to PasteShapeCommand

--- src/main/PasteShapeCommand.java
+++ src/main/PasteShapeCommand.java
@@ -1,17 +1,24 @@
 package main;
 
 
+import java.util.ArrayList;
 
 public class PasteShapeCommand implements ICommand, IUndoRedo{
 
     ShapeList shapelist =  ShapeList.getInstance();
+    ArrayList<IShape> copied = new ArrayList<>();
 
 
 
     @Override
     public void runCommand() {
         System.out.println("pasted");
+        for (int i = 0; i < shapelist.copied.size(); i++) {
+            this.copied.add(shapelist.copied.get(i));
+        }
+
         shapelist.addNewShape(shapelist.copied);
+
         System.out.println(shapelist.shapeListsize());
         shapelist.undrawAll();
         shapelist.drawAll();
@@ -21,6 +28,7 @@ public class PasteShapeCommand implements ICommand, IUndoRedo{
     @Override
     public void undoCommand() {
         System.out.println("undo pasted");
+        shapelist.removeCopied(this.copied);
         shapelist.undrawAll();
         shapelist.drawAll();
 
@@ -28,6 +36,11 @@ public class PasteShapeCommand implements ICommand, IUndoRedo{
 
     @Override
     public void redoCommand() {
+        for (int i = 0; i < this.copied.size(); i++) {
+            shapelist.addShape(copied.get(i));
+        }
+        shapelist.undrawAll();
+        shapelist.drawAll();
         System.out.println("redo pasted");
 
     }
