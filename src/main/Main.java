9bdf5c4777061532fc84bb071e67edf612c90dc3
salemij93 <salemi-j@hotmail.com>
2021-03-06 09:10:27 -0600

    adding graphics2d to Shapelist

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,7 +17,6 @@ public class Main {
         PaintCanvasBase paintCanvas = new PaintCanvas();
         IGuiWindow guiWindow = new GuiWindow(paintCanvas);
         IUiModule uiModule = new Gui(guiWindow);
-        ShapeList shapelist =  ShapeList.getInstance();
         ApplicationState appState = new ApplicationState(uiModule);
         IJPaintController controller = new JPaintController(uiModule, appState);
         controller.setup();
