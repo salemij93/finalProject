881310e1c1099e8331ea847d96d63581573e15aa
salemij93 <salemi-j@hotmail.com>
2021-03-06 12:28:35 -0600

    making groups work first

--- src/main/ShapeList.java
+++ src/main/ShapeList.java
@@ -4,8 +4,7 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+
 
 
 public class ShapeList {
