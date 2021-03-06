0c13a097c535eb223b2593bcab13cb1852f2b06e
salemij93 <salemi-j@hotmail.com>
2021-03-06 13:40:52 -0600

    adding GroupShape

--- src/main/ShapeList.java
+++ src/main/ShapeList.java
@@ -240,6 +240,10 @@ public class ShapeList {
     }
 
     public void groupShapes(){
+        if(shapes.size() >0){
+            GroupShape newGroup = new GroupShape(shapes.get(0).getGraphics2d());
+
+        }
         for (int i = 0; i < shapes.size(); i++) {
             IShape shape = shapes.get(i);
             if (shape.getState()){
