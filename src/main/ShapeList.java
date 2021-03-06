f4c667e764bdfd541e6e1ca84092d54b1498e20c
salemij93 <salemi-j@hotmail.com>
2021-03-06 15:03:31 -0600

    adding GroupShape

--- src/main/ShapeList.java
+++ src/main/ShapeList.java
@@ -252,13 +252,13 @@ public class ShapeList {
                     minX = shape.getX1();
                 }
                 if(shape.getX2() > maxX){
-                    maxX = shape.getX2()+shape.getX1();
+                    maxX = shape.getX2()+minX + shape.getX1();
                 }
                 if(shape.getY1() < minY){
                     minY = shape.getY1();
                 }
                 if(shape.getY2() > maxY) {
-                    maxY = shape.getY2()+shape.getY1();
+                    maxY = shape.getY2()+minY+ shape.getY1();
                 }
                 if (shape.getState()){
                     newGroup.addShapeToGroup(shape);
