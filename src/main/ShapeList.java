167b35a2061329826e933fbb520db8edfbabebe7
salemij93 <salemi-j@hotmail.com>
2021-03-07 12:44:50 -0600

    adding GroupShape

--- src/main/ShapeList.java
+++ src/main/ShapeList.java
@@ -219,7 +219,7 @@ public class ShapeList {
         for (int i = 0; i < shapeList.size(); i++) {
             IShape shape = shapeList.get(i);
             switch (shape.getshapeName()) {
-                case "RECTANGLE", "ELLIPSE" -> {
+                case "RECTANGLE", "ELLIPSE", "Group" -> {
                     shape.setX1(shape.getX1());
                     shape.setY1(shape.getY1() );
                 }
